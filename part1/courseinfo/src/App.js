c71557d6d174b3172a01b50c2b26bd86a35362fe
Jesus Yepez <64677783+yepezdj@users.noreply.github.com>
2023-05-01 16:07:12 -0500

    exercise 1.5 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,12 +1,22 @@
 const App = () => {
   //Constant
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   function Headers(props) {
     return <h1>{props.course}</h1>;
@@ -24,41 +34,31 @@ const App = () => {
   function Content(props) {
     return (
       <div>
-        <Part part={props.part1} exercises={props.exercises1} />
-        <Part part={props.part2} exercises={props.exercises2} />
-        <Part part={props.p3.part3} exercises={props.p3.exercises3} />
+        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
       </div>
     );
   }
 
   function Total(props) {
-    return (
-      <p>
-        Number of exercises{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
-    );
+    let sum = 0;
+    props.parts.forEach(function (num) {
+      console.log(num);
+      sum += num.exercises; // equivalent to: sum = sum + num;
+    });
+
+    return <p>Number of exercises {sum}</p>;
   }
 
   //Main funtion
   return (
     <div>
-      <Headers course={course} />
+      <Headers course={course.name} />
       <br></br>
-      {/* p3 is sent as an object */}
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        p3={{ part3: part3, exercises3: exercises3 }}
-      />
+      <Content parts={course.parts} />
       <br></br>
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Total parts={course.parts} />
     </div>
   );
 };
