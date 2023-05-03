5efebb4a948bc7835ff8a1b50bcb3f216c49da54
Jesus Yepez <64677783+yepezdj@users.noreply.github.com>
2023-05-03 17:43:51 -0500

    just testing

--- part1/testapp/src/App.js
+++ part1/testapp/src/App.js
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  console.log('rendering...', counter)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
   )
 }
 
-export default App
+export default App;
